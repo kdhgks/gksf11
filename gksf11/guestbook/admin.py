f4c2d071d7133f9fc04ecd4d6fe30b31591d3454
o_lnvvl <lnvvl1202@naver.com>
2025-05-06 02:42:18 +0900

    Feat: db 인터페이스 수정

--- gksf11/guestbook/admin.py
+++ gksf11/guestbook/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import GuestMessage
 
-admin.site.register(GuestMessage)
+@admin.register(GuestMessage)
+class GuestMessageAdmin(admin.ModelAdmin):
+    list_display = ('author', 'message', 'written_at')
--- gksf11/preapply/admin.py
+++ gksf11/preapply/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Application
 
-admin.site.register(Application)
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'sessions')  # 또는 sessions_list 등
